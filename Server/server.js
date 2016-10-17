95953ad7f65b7a6186c06b0b8a30dac54b017c9b
Victor Mu <vic.mgs@gmail.com>
2016-10-17 12:06:57 -0700

    Add client dependencies bower, add index page

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/f1', function (req, res) {
+  request('https://www.formula1.com/en/results.html/2016/drivers.html', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
+    }
+  });
+});
+
+app.get('/motogp', function (req, res) {
+  request('http://www.motogp.com/en/Results+Statistics/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
+    }
+  });
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Listening on port 3000!');
 });
