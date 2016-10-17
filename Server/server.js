a7da6d9b5453ebc243f485536d150e35d2448e94
Victor Mu <vic.mgs@gmail.com>
2016-10-17 16:18:23 -0700

    pull f1 results

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request');
+var cheerio = require('cheerio');
+
 
 app.use('/', express.static('./client'));
 
@@ -12,15 +14,40 @@ app.get('/', function(req, res){
 app.get('/f1', function (req, res) {
   request('https://www.formula1.com/en/results.html/2016/drivers.html', function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var table = $('.resultsarchive-wrapper').html();
+
+      res.status(200).send(table);
+    }
+  });
+});
+
+app.get('/wtcc', function (req, res) {
+  request('http://www.fiawtcc.com/standings/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
       res.status(200).send(body);
-      var standings = new Standings(body)
-      res.status(200).send(standings.render());
+    }
+  });
+});
+
+app.get('/wrc', function (req, res) {
+  request('http://www.wrc.com/en/wrc/results/championship/drivers/page/193-290---.html', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
     }
   });
 });
 
 app.get('/motogp', function (req, res) {
   request('http://www.motogp.com/en/Results+Statistics/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
+    }
+  });
+});
+
+app.get('/wsbk', function (req, res) {
+  request('http://www.worldsbk.com/en/results%20statistics', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.status(200).send(body);
     }
